1e7f0b3baaa99aa4726e9d561fbf65a8098caf4d
adrika <166372847+Aadrika8@users.noreply.github.com>
2025-03-14 00:42:04 +0530

    Create new_hello.cpp

---
+++ new_hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from the new C++ file!" << endl;
+    cout << "Jenkins Pipeline is working fine!" << endl;
+    return 0;
+}
