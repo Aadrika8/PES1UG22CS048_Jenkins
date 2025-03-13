fa443d8ec67404a46eeadddeb501145cc4dd7e6e
Adrika <singhaadrika8@gmail.com>
2025-03-14 00:23:51 +0530

    Updated hello.cpp with a new print statement

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,20 @@
+<<<<<<< HEAD
+=======
+// File: hello.cpp
+>>>>>>> a275600 (updated)
 #include <iostream>
 using namespace std;
 
 int main(void) {
+<<<<<<< HEAD
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "Webhook is working!\n";  // New line
+=======
+  cout << "Hello, World!\n";
+  cout << "Hello, Jenkins!\n";
+  cout << "I have successfully triggered a build via Webhook!\n";  // <-- Changed message
+>>>>>>> a275600 (updated)
   return 0;
 }
