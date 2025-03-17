eb00323c50f8a827cd8ca0d0bc6f13844e87590c
adrika <166372847+Aadrika8@users.noreply.github.com>
2025-03-17 10:47:14 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-=======
-// File: hello.cpp
->>>>>>> a275600 (updated)
 #include <iostream>
-using namespace std;
 
-int main(void) {
-<<<<<<< HEAD
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  cout << "Webhook is working!\n";  // New line
-=======
-  cout << "Hello, World!\n";
-  cout << "Hello, Jenkins!\n";
-  cout << "I have successfully triggered a build via Webhook!\n";  // <-- Changed message
->>>>>>> a275600 (updated)
-  return 0;
+int main() {
+    std::cout << "Hello, Jenkins Webhook Test!" << std::endl;
+    return 0;
 }
+
