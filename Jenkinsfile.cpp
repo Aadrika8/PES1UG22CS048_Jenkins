b7421dd6d78fd7eb1a4ab1489332d7f2c5a2d73a
adrika <166372847+Aadrika8@users.noreply.github.com>
2025-03-17 18:27:05 +0530

    Create Jenkinsfile.cpp

---
+++ Jenkinsfile.cpp
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'g++ -o YOUR_SRN-1 hello.cpp'
+                echo 'Build Stage Successful'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh './YOUR_SRN-1'
+                echo 'Test Stage Successful'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo 'Deployment Successful'
+            }
+        }
+    }
+
+    post {
+        failure {
+            echo 'Pipeline failed'
+        }
+    }
+}
